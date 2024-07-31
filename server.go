fd20a7228fb1ea881b8bd58f5fc572a880737eef
Keke2004 <168921198+Keke2004@users.noreply.github.com>
2024-07-31 15:02:50 +0800

    Create server.go

---
+++ server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	pb "kvstore/kv/kvs"
+	"log"
+	"net"
+	"sync"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedKeyValueStoreServer
+	store map[string]string
+	mu    sync.Mutex
+}
+
+func (s *server) Set(ctx context.Context, in *pb.SetRequest) (*pb.SetResponse, error) {
+	s.mu.Lock()
+	s.store[in.Key] = in.Value
+	s.mu.Unlock()
+	return &pb.SetResponse{Success: true}, nil
+}
+
+func (s *server) Get(ctx context.Context, in *pb.GetRequest) (*pb.GetResponse, error) {
+	s.mu.Lock()
+	value, err := s.store[in.Key]
+	s.mu.Unlock()
+	if !err {
+		return &pb.GetResponse{Value: ""}, nil
+	}
+	return &pb.GetResponse{Value: value}, nil
+}
+
+func (s *server) Delete(ctx context.Context, in *pb.DeleteRequest) (*pb.DeleteResponse, error) {
+	s.mu.Lock()
+	delete(s.store, in.Key)
+	s.mu.Unlock()
+	return &pb.DeleteResponse{Success: true}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	kvServer := &server{store: make(map[string]string)}
+	pb.RegisterKeyValueStoreServer(s, kvServer)
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
